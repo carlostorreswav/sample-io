63bcaeb2d6c03790055ce64f9dd13f3aea78a642
Carlos Torres <carlos.torres@secuoyas.com>
2022-04-19 18:44:53 +0200

    Feature: PREVIEW CHORDS

--- src/clouds/ToneCloud.js
+++ src/clouds/ToneCloud.js
@@ -15,10 +15,29 @@ const ToneMain = styled.div`
   border-radius: 4px;
 `
 
+const CustomCard = styled(CloudCard)`
+  padding: 4px 8px;
+  color: ${p => (p.activated ? "#FFF" : "#FFFFFF44")};
+  border: 1px solid black;
+  margin: 2px;
+  cursor: pointer;
+`
+
 const ToneCloud = () => {
   const { Clouds, CloudFilter } = useContext(ToneContext)
   const [open, setOpen] = useState(true)
-  const handleClick = array => {
+  const [preview, setPreview] = useState({ activated: true, chords: true, bass: false })
+
+  const ChordPreview = tone => {
+    const sharpTypo = tone.label.replace("#", "S")
+    const mySound = require(`../chords/${preview.chords ? "" : "b_"}${sharpTypo}.mp3`)
+    const audio = new Audio(mySound)
+    audio.volume = 1
+    audio.play()
+  }
+
+  const handleClick = (array, tone) => {
+    preview.activated && ChordPreview(tone)
     CloudFilter(array)
   }
 
@@ -32,14 +51,14 @@ const ToneCloud = () => {
         {Clouds?.Tones?.map((Tone, idx) => (
           <ToneMain key={idx}>
             <CloudCard
-              onClick={() => handleClick(Tone.major.ids)}
+              onClick={() => handleClick(Tone.major.ids, Tone.major)}
               hasSamples={Tone?.major?.ids?.length > 0}
             >
               <CloudCardName>{Tone.major.label}</CloudCardName>
               <CloudCardNumber>{Tone?.major?.ids?.length || 0}</CloudCardNumber>
             </CloudCard>
             <CloudCard
-              onClick={() => handleClick(Tone.minor.ids)}
+              onClick={() => handleClick(Tone.minor.ids, Tone.minor)}
               hasSamples={Tone?.minor?.ids?.length > 0}
             >
               <CloudCardName>{Tone.minor.label}</CloudCardName>
@@ -47,6 +66,38 @@ const ToneCloud = () => {
             </CloudCard>
           </ToneMain>
         ))}
+      </CloudFlex>
+      <CloudFlex open={open}>
+        <CustomCard
+          activated={preview.activated}
+          onClick={() =>
+            preview.activated
+              ? setPreview(p => ({ ...p, activated: false, chords: false, bass: false }))
+              : setPreview(p => ({ ...p, activated: true, chords: true, bass: false }))
+          }
+        >
+          <CloudCardName>Preview</CloudCardName>
+        </CustomCard>
+        <CustomCard
+          activated={preview.chords}
+          onClick={() =>
+            preview.activated
+              ? setPreview(p => ({ ...p, chords: !p.chords, bass: !p.bass }))
+              : setPreview(p => ({ ...p, activated: true, chords: true, bass: false }))
+          }
+        >
+          <CloudCardName>Chords</CloudCardName>
+        </CustomCard>
+        <CustomCard
+          activated={preview.bass}
+          onClick={() =>
+            preview.activated
+              ? setPreview(p => ({ ...p, chords: !p.chords, bass: !p.bass }))
+              : setPreview(p => ({ ...p, activated: true, chords: false, bass: true }))
+          }
+        >
+          <CloudCardName>Bass</CloudCardName>
+        </CustomCard>
       </CloudFlex>
     </Module>
   )
